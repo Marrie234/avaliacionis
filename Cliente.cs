f08d2bab211036cf927538229b2870fb5f645345
Marrie~ <majucappai1@gmail.com>
2026-08-30 14:09:01 -0300

    pq eu sinto que esta dando errado? estou eu biruleibe?

--- Cliente.cs
+++ Cliente.cs
@@ -2,3 +2,12 @@ class Cliente
 {
 private string? nome;
 private string telefone;
+//construtor simples tbm
+  public Cliente
+  {
+    this.nome = nome;
+    this.telefone = telefone;
+  }
+  public strin? Nome
+  {
+    get
