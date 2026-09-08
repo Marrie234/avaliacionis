18f6b56803fdb18108cd72195171dcb2bab374b9
Marrie~ <majucappai1@gmail.com>
2026-09-08 15:50:52 -0300

    sla tbm

--- Cliente.cs
+++ Cliente.cs
@@ -1,28 +1,29 @@
 class Cliente
 {
-private string? nome;
-private string telefone;
-//construtor simples tbm
-  public Cliente
-  {
-    //o this serve para o C# não se baralhar
-    this.nome = nome;
-    this.telefone = telefone;
-  }
-  public string? Nome
-  {
-    get{return sabor;}
-    set{sabor = value;}
-  }
-  public string? Telefone
-  {
-    //o get retorna o valor e o set altera o valor
-    get { return telefone; }
-    set { telefone = value; }
-  }
-  //a ToString devolve uma frase organizada com o nome e o preço formatados e mais bunitim :).
-  public override string ToString()
-  {
-     return $"[CLIENTE] Nome: {nome} | Telefone: {telefone}";
-  }
+    private string? nome;
+    private int telefone; // Alterado para int
+
+    // Construtor atualizado pra int
+    public Cliente(string? nome, int telefone)
+    {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public string? Nome
+    {
+        get { return nome; }
+        set { nome = value; }
+    }
+
+    public int Telefone // Alterado para int
+    {
+        get { return telefone; }
+        set { telefone = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"[CLIENTE] Nome: {nome} | Telefone: {telefone}";
+    }
 }
